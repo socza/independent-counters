ce36cf46ba08c2f97b7375d080d796701c40ca2a
Piotr Sociński <psocinski@gmail.com>
2018-02-05 10:35:45 +0100

    Finished tas

--- script.js
+++ script.js
@@ -8,6 +8,7 @@ var Counter = React.createClass({
 
 	},
 
+
 	increment: function() {
 		this.setState({
 			counter: this.state.counter + 1
@@ -22,15 +23,37 @@ var Counter = React.createClass({
 
 	}, 
 
+
 	render: function() {
 
 		return React.createElement('div', {},
-			React.createElement('span', {}, 'Licznik ' + this.state.counter),
 			React.createElement('button', {onClick: this.increment}, 'Dodaj'),
-			React.createElement('button', {onClick: this.decrement}, 'Odejmij')
+			React.createElement('span', {}, '  Licznik  ' + this.state.counter + ' '),
+			React.createElement('button', {onClick: this.decrement}, ' ' + 'Odejmij')
 		);	
 	
-	}
+	},
+
+	componentWillMount: function(mount) {
+		console.log('Montuję render');
+	},
+
+	componentDidMount: function () {
+		console.log('Render wyświetlony');
+	},
+
+	shouldComponentUpdate: function(nextProps, nextState) {
+    	console.log('Pobieram z dane serwera np. Dane poprzez API');
+   	return true;
+   	},
+
+	componentWillUpdate(nextProps, nextState) {
+    	console.log('Dane z serwera zaktualizowane. Wyświetlam po dane API po aktualizacji');
+   	},
+
+    componentWillUnmount: function() {
+    	console.log('Dane z serwera usunięte');
+    }
 
 });
 
@@ -39,7 +62,7 @@ var App = React.createClass({
 	render: function() {
 
 		return (
-			React.createElement('div', {className: 'app'},
+			React.createElement('div', {},
 				React.createElement(Counter, {}),
 				React.createElement(Counter, {}),
 				React.createElement(Counter, {}),
